ede5fd8c42c805ee908921c2477d768e10650304
vsevalot <vsevolod.tetervak@tripleten.com>
2025-01-27 18:52:46 +0100

    removed ruff as second linter

--- nvim/lua/vsevalot/init.lua
+++ nvim/lua/vsevalot/init.lua
@@ -1,6 +1,9 @@
 require("vsevalot.core")
 require("vsevalot.lazy")
-vim.o.langmap = 'ФИСВУАПРШОЛДЬТЩЗЙКЫЕГМЦЧНЯ;ABCDEFGHIJKLMNOPQRSTUVWXYZ,фисвуапршолдьтщзйкыегмцчня;abcdefghijklmnopqrstuvwxyz'
+
+vim.opt.wrap = false
+vim.o.langmap =
+  "ФИСВУАПРШОЛДЬТЩЗЙКЫЕГМЦЧНЯ;ABCDEFGHIJKLMNOPQRSTUVWXYZ,фисвуапршолдьтщзйкыегмцчня;abcdefghijklmnopqrstuvwxyz"
 return {
   "nvim-lua/plenary.nvim", -- lua functions that many plugins use
 }
