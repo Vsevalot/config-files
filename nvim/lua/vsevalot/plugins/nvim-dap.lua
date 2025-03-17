4eed3787b743218463a42154cd772f4e8a9212aa
vsevalot <vsevolod.tetervak@tripleten.com>
2025-03-17 17:37:31 +0100

    debuger etc

---
+++ nvim/lua/vsevalot/plugins/nvim-dap.lua
@@ -0,0 +1,83 @@
+return {
+  {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+      "nvim-neotest/nvim-nio",
+      "rcarriga/nvim-dap-ui",
+      "mfussenegger/nvim-dap-python",
+    },
+    config = function()
+      local dap = require("dap")
+      local dapui = require("dapui")
+      local dap_python = require("dap-python")
+
+      require("dapui").setup({})
+
+      dap_python.setup("python3")
+
+      vim.fn.sign_define("DapBreakpoint", {
+        text = "",
+        texthl = "DiagnosticSignError",
+        linehl = "",
+        numhl = "",
+      })
+
+      vim.fn.sign_define("DapBreakpointRejected", {
+        text = "", -- or "❌"
+        texthl = "DiagnosticSignError",
+        linehl = "",
+        numhl = "",
+      })
+
+      vim.fn.sign_define("DapStopped", {
+        text = "", -- or "→"
+        texthl = "DiagnosticSignWarn",
+        linehl = "Visual",
+        numhl = "DiagnosticSignWarn",
+      })
+
+      -- Automatically open/close DAP UI
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open()
+      end
+
+      local opts = { noremap = true, silent = true }
+
+      -- Toggle breakpoint
+      vim.keymap.set("n", "<leader>db", function()
+        dap.toggle_breakpoint()
+      end, opts)
+
+      -- Continue / Start
+      vim.keymap.set("n", "<leader>dc", function()
+        dap.continue()
+      end, opts)
+
+      -- Step Over - just step to the next line
+      vim.keymap.set("n", "<leader>ds", function()
+        dap.step_over()
+      end, opts)
+
+      -- Step Into
+      vim.keymap.set("n", "<leader>di", function()
+        dap.step_into()
+      end, opts)
+
+      -- Step Out - go outside of the called function
+      vim.keymap.set("n", "<leader>do", function()
+        dap.step_out()
+      end, opts)
+
+      vim.keymap.set("n", "<leader>du", function()
+        dap.up()
+      end, opts)
+      vim.keymap.set("n", "<leader>dd", function()
+        dap.down()
+      end, opts)
+      -- Toggle DAP UI
+      vim.keymap.set("n", "<leader>dU", function()
+        dapui.toggle()
+      end, opts)
+    end,
+  },
+}
